1d77814bc4d68e4398a1dc67399ea278b1a44bff
--replace-all <--replace-all>
2026-04-21 19:38:16 +0200

    commit inicial

---
+++ com/projecte/main/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+package com.projecte.main;
+
+public class ProgramaPrincipal {
+   public static void main(String[] args) {
+      inici();
+   }
+
+   private static void inici() {
+      // Ivan.mostrarNom();
+      // Andreu.mostrarNom();
+      // Iker.mostrarNom();
+   }
+}
