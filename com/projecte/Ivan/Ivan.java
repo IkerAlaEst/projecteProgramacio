d89bd6fc4d8fdbd04d7dff78e5f3a9e305ea21e5
IvanCalatayud <ivacaltor@alu.edu.gva.es>
2026-04-22 15:32:35 +0200

    Paquet Ivan i Metodes

---
+++ com/projecte/Ivan/Ivan.java
@@ -0,0 +1,7 @@
+package com.projecte.Ivan;
+
+public class Ivan {
+    static public void mostrarNom() {
+        System.out.println("Ivan Calatayud Toral");
+    }
+}
--- com/projecte/main/ProgramaPrincipal.java
+++ com/projecte/main/ProgramaPrincipal.java
@@ -1,12 +1,14 @@
 package com.projecte.main;
 
+import com.projecte.Ivan.Ivan;
+
 public class ProgramaPrincipal {
    public static void main(String[] args) {
       inici();
    }
 
    private static void inici() {
-      // Ivan.mostrarNom();
+      Ivan.mostrarNom();
       // Andreu.mostrarNom();
       // Iker.mostrarNom();
    }
